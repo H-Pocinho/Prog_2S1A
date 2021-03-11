2f8ca4d717bdbd737396d019e9cde695d6b1af19
Pocinho <hapocinho@gmail.com>
2021-03-11 19:24:46 +0000

    add 6

--- Lab_1/ex6.c
+++ Lab_1/ex6.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int m;
+int m, cs, ano;
 
 int main(){
     do{
@@ -10,6 +10,31 @@ int main(){
             printf("Numero invalido\n");
         }
     } while ( m<1 || m>12 );
+
+    if ( (m==1) || (m==3) || (m==5) || (m==7) || (m==8) || (m==10) || (m==12))
+    	cs=0;
+    else if (m==2)
+        cs=2;
+    else
+        cs=1;
+    
+
+    switch(cs){
+        case 0:
+            printf("O mes tem 31 dias\n");
+            break;
+        case 1:
+            printf("O mes tem 30 dias\n");
+            break;
+        case 2:
+            printf("Introduza o ano: ");
+            scanf("%d", &ano);
+            if ((((ano%4 == 0) && (ano%100 != 0) ) || (ano%400 == 0))==1)
+                printf("O mes tem 29 dias\n");
+            else
+                printf("O mes tem 28 dias\n");
+        break;
+    }
     
  return 0;   
 }
