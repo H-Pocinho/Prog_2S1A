5e9874cef910ac2ef9d90c057c7800e26ab47e0b
Pocinho <hapocinho@gmail.com>
2021-04-02 13:19:56 +0100

    add ex5

---
+++ Lab_2/ex5.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void FlagMultipleNumbers(int i, int j , int numbers[]){
+    if (i>j && numbers[i]%j==0)
+    {
+        numbers[i]=0;
+    }
+}
+
+
+void SieveOfEratosthenes(int n){
+int i,j,limit;
+int numbers[1000];
+
+    limit=sqrt(n);
+
+
+    for ( i = 0; i < n; i++)
+    {
+        numbers[i]=i+2;
+    }
+
+
+    for ( j = 2; j < limit; j++)
+    {
+        for ( i = 0; i < n; i++)
+        {
+            FlagMultipleNumbers(i,j,numbers);
+        }
+    }
+    for ( i = 0; i < (n-1); i++)
+    {
+        if (numbers[i]!=0)
+        {
+            printf("%d\n",numbers[i]);
+        }
+        
+    }
+    
+}
+
+
+
+int main(){
+    int n;
+
+    printf("Introduza o numero maximo:\n");
+    if (scanf("%d", &n) != 1){
+        printf("Failled to read STDIN!\n");
+        exit(0);
+    }
+    SieveOfEratosthenes(n);
+    return 0;
+}
